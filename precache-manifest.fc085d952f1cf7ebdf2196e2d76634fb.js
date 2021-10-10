fab847df70b6678365480bcfb969543c27091b0b
ordamari <ordamari971@gmail.com>
2021-10-10 18:45:28 +0300

    Updates

---
+++ precache-manifest.fc085d952f1cf7ebdf2196e2d76634fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530c73fb8d8deeb28b73692266cb5f5d",
+    "url": "/protfolio2/index.html"
+  },
+  {
+    "revision": "cead8dcbf3b0dd691ebe",
+    "url": "/protfolio2/static/css/main.7cae5891.chunk.css"
+  },
+  {
+    "revision": "6f05690305cf4365c295",
+    "url": "/protfolio2/static/js/2.4603beef.chunk.js"
+  },
+  {
+    "revision": "41e7baf30a8acb9a1cc0c439643acc10",
+    "url": "/protfolio2/static/js/2.4603beef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cead8dcbf3b0dd691ebe",
+    "url": "/protfolio2/static/js/main.7d51a0b0.chunk.js"
+  },
+  {
+    "revision": "5707bf700998f0307c84",
+    "url": "/protfolio2/static/js/runtime-main.d34b8a1b.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/protfolio2/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/protfolio2/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c37a496b1725365b54a91b30e9f67eab",
+    "url": "/protfolio2/static/media/bg.c37a496b.jpg"
+  },
+  {
+    "revision": "461dd45682e5b12cf17ae5849d956afb",
+    "url": "/protfolio2/static/media/me.461dd456.jpg"
+  }
+]);
